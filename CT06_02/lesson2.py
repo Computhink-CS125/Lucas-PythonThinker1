823c07053c2bea8ba862b8841edb64beedc79c78
Computhink-CS125 <cs125@computhink.com.sg>
2025-01-18 15:32:37 +0800

    Update lesson2.py
    skibidi lesson 2 from CompuThink
    
    
    User: CS125

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -4,4 +4,58 @@ print("Hello from lesson 2")
 # Using comments, translate the code shown on screen into pseudocode.
 
 ######## Write the pseudocode in comments for task 3 here
-# Using comments, translate the code shown on screen into pseudocode.
+# Using comments, translate the code shown on screen into pseudocode
+# ask user for their age
+# store user input into "answer"
+# if "answer" is less than 18
+#   say "Access Denied"
+# or else
+#   say "Welcome!"
+
+
+  # repeat 10 times
+  # print "hey!"
+  # Take 10 steps to the right
+
+  set counter to 0
+repeat until 
+
+#   ask user "what is your age"
+# if answer is less than 18,
+# say "Access Denied!"
+# otherwise
+# say "Welcome!"
+
+Task("is this plastic")
+if it is glass
+assign glass to glass bin
+if it is paper
+assign paper to paper bin
+otherwise
+assign to plastic bin
+
+
+  # create variable phrase
+  # set variable phrase as "passme"
+  # if user entered the wrong phrase
+  # say "Invalid phrase!"
+  # else
+  # say "Phrase correct! welcome!"
+
+
+
+
+
+create variable test1
+create variable test2
+create variable test3
+task ("what is the final score")
+(test1 x 20%) + (test2 x 40%) x (test3 x 40%)
+
+test1 = 70
+test2 = 80
+test3 = 85
+
+
+
+
