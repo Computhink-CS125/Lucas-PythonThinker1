e016231fc53bcc180947a45ac169a39f09e52b4a
Computhink-CS125 <cs125@computhink.com.sg>
2025-02-15 06:00:59 +0000

    Feb 15, 2025, 6:00 AM

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,1 +1,100 @@
-print("Hello from lesson 3")
+# print("Hello from lesson 3")
+
+
+# create variable Adam
+# create variable Brandon
+# create variable Charlie
+# create variable Daniel
+# create variable Average_Score
+# create variable Total_Score
+
+# set value for Adam as 60
+# set value for Brandon as 58
+# set value for Charlie as 98
+# set value for Daniel as 79
+
+# Total_Score = Adam + Brandon + Charlie + Daniel
+
+# print value of Total_Score
+
+# Average_score = Total_Score / 4
+
+# print value of Average_Score
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# create variable age
+
+# set variable age as 67
+
+# print out age
+
+
+# X = 10
+
+# X = 20
+
+# print "X'
+
+
+
+
+
+
+# variable1 = 2
+# variable2 = 483
+
+# sum1 = variable1 + variable2
+# product = variable1 * variable2
+# division = variable1 / variable2
+# subtraction = variable1 - variable2
+
+# print (sum1)
+
+
+
+
+# set variable1 = 17
+
+# set variable2 = 15263748
+
+
+# a = 6231472984048742380450924857234075438095742397230843702
+
+# b = 934520834709384562973857934758695872456923845785437489
+
+# sum2 = a + b
+
+# print (sum2)
+
+
+
+
+
+# variable1of1 = 10
+# variable2of1 = 50
+# variable3of1 = 3
+# subtraction = variable2of1 - variable1of1
+# ans = subtraction * variable3of1
+# print (ans)
+
+
+
+
+print
