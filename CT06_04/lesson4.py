11b46b19dd9b8e966ff8458610d7383eaff26f83
Computhink-CS125 <cs125@computhink.com.sg>
2025-02-15 06:37:28 +0000

    Feb 15, 2025, 6:37 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -2,7 +2,6 @@ age = int(input("What is your current age? "))
 age = age + 1
 print("Next year you will be " + str(age) + " years old")
 
-
 numbuer = input("Choose any number. ")
 numbuer = numbuer * numbuer
 print("Nah, I'd number " + str(numbuer))
