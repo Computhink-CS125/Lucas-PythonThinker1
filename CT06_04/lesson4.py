7ee080079173cac21e482b4281a228e03108208d
Computhink-CS125 <cs125@computhink.com.sg>
2025-02-15 06:36:40 +0000

    Feb 15, 2025, 6:36 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -4,5 +4,5 @@ print("Next year you will be " + str(age) + " years old")
 
 
 numbuer = input("Choose any number. ")
-numbuer = numbuer*2
+numbuer = numbuer * 2
 print("Nah, I'd number " + str(numbuer))
