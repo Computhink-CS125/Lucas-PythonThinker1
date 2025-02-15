aa9f780581c07e75a39dff360ebf8a62a95f373f
Computhink-CS125 <cs125@computhink.com.sg>
2025-02-15 06:42:02 +0000

    Feb 15, 2025, 6:42 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,7 +1,14 @@
-age = int(input("What is your current age? "))
-age = age + 1
-print("Next year you will be " + str(age) + " years old")
+# age = int(input("What is your current age? "))
+# age = age + 1
+# print("Next year you will be " + str(age) + " years old")
 
-numbuer = input("Choose any number. ")
-numbuer = numbuer * numbuer
+# numbuer = input("Choose any number. ")
+# numbuer = numbuer * numbuer
 # print("Nah, I'd number " + str(numbuer))
+
+
+
+
+
+
+
