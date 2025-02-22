3f5355f3334c9a0aa2d3016c8e93656102a085be
Computhink-CS125 <cs125@computhink.com.sg>
2025-02-22 07:13:27 +0000

    Feb 22, 2025, 7:13 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -6,3 +6,25 @@ for i in range(student):
     score = score + int(input("What is the student's marks?"))
 
 print(score / student)
+
+
+
+## Task 1: Syntax Errors
+# Fix the errors in the following:
+
+# **Task 1a**:
+for i in range(3):
+    print("Hello, World!")
+
+# **Task 1b**:
+for i in range(5):
+    print(i)
+
+# **Task 1c**:
+print("Hello, World!")
+
+# **Task 1d**:
+ 5
+
+# **Task 1e**:
+print ("Hello, World!")
