ff3320344bff23daae4f7aa45ced701b185bb00b
Computhink-CS125 <cs125@computhink.com.sg>
2025-02-22 06:53:50 +0000

    Feb 22, 2025, 6:53 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,6 +1,4 @@
 student = int(input("How many students did the test?"))
 for i in range(student):
 
-    score = score + int(input("What is the student's marks?"))
-
-print(f"iofewif)
+    score = score + int(input("What is the 
