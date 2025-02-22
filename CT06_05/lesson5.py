1694388d4f8832ed7224410f2175346df4570733
Computhink-CS125 <cs125@computhink.com.sg>
2025-02-22 06:38:56 +0000

    Feb 22, 2025, 6:38 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -22,8 +22,8 @@ startingtons = input("What integer would you like to start with?")
 endingtons = input("What integer would you like to end with?")
 
 if endingtons > startingtons:
-    for i in range(startingtons, endingtons, -1):
+    for i in range(startingtons, endingtons, 1):
         print(i)
 else:
-    for i in range(startingtons, endingtons, 1):
+    for i in range(startingtons, endingtons, -1):
         print(i)
