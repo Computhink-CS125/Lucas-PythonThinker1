e517b85a4edd6a55d20ceadfb6b7bb507548021a
Computhink-CS125 <cs125@computhink.com.sg>
2025-02-22 07:27:42 +0000

    Feb 22, 2025, 7:27 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,53 +1,77 @@
-# # student = int(input("How many students did the test?"))
-# # score = 0
+# # # student = int(input("How many students did the test?"))
+# # # score = 0
 
-# # for i in range(student):
+# # # for i in range(student):
 
-# #     score = score + int(input("What is the student's marks?"))
+# # #     score = score + int(input("What is the student's marks?"))
 
-# # print(score / student)
+# # # print(score / student)
 
 
 
-# ## Task 1: Syntax Errors
-# # Fix the errors in the following:
+# # ## Task 1: Syntax Errors
+# # # Fix the errors in the following:
 
-# # **Task 1a**:
-# for i in range(3):
-#     print("Hello, World!")
+# # # **Task 1a**:
+# # for i in range(3):
+# #     print("Hello, World!")
 
-# # **Task 1b**:
-# for i in range(5):
-#     print(i)
+# # # **Task 1b**:
+# # for i in range(5):
+# #     print(i)
 
-# # **Task 1c**:
+# # # **Task 1c**:
+# # print("Hello, World!")
+
+# # # **Task 1d**:
+# # i = 5
+
+# # # **Task 1e**:
+# # print ("Hello, World!")
+
+
+
+
+
+
+
+
+
+# # **Task 2a**:
+# age = 10
+# print(age)
+
+# # **Task 2b**:
+# name = "Alice"
+# print(name)
+
+# # **Task 2c**:
+# x = 5
+# print(x)
+
+# # **Task 2d**:
 # print("Hello, World!")
 
-# # **Task 1d**:
-# i = 5
+# **Task 3a**:
+age = "25"
+print(age + 1)
 
-# # **Task 1e**:
-# print ("Hello, World!")
+# **Task 3b**:
+number = 10
+print(number - "5")
 
+# **Task 3c**:
+print("Repeat" * "3")
 
+# **Task 3d**:
+year = 2023
+print("The year is " + year)
 
+# **Task 3e**:
+x = "10"
+y = x / 2
 
-
-
-
-
-
-# **Task 2a**:
-age = 10
-print(age)
-
-# **Task 2b**:
-name = "Alice"
-print(name)
-
-# **Task 2c**:
-x = 5
-print(x)
-
-# **Task 2d**:
-print("Hello, World!")
+# **Task 3f**:
+end = "5"
+for i in range(end):
+    print(i)
