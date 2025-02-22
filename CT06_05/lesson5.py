db630ce1532a22bbcfcaf33a0889a4f0536179ac
Computhink-CS125 <cs125@computhink.com.sg>
2025-02-22 06:26:36 +0000

    Feb 22, 2025, 6:26 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,4 +1,4 @@
-# # # we print the for loop to printingtons the numba from 2 too 24!!! in multiples of 2!!!!!!!!!!!! hehehaha
+# # # # we print the for loop to printingtons the numba from 2 too 24!!! in multiples of 2!!!!!!!!!!!! hehehaha
 
 
 
@@ -7,12 +7,12 @@
 
 
 
-# # for i in range(2, 25, 2):
-# #     print(i)
+# # # for i in range(2, 25, 2):
+# # #     print(i)
 
-# for e in range(8, 97, 8):
-#     print(e)
+# # for e in range(8, 97, 8):
+# #     print(e)
 
 
-for d in range(5, 0, -1):
-    print(d)
+# for d in range(5, 0, -1):
+#     print(d)
