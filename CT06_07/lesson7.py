3b8db624256d9853dbd1a86e7990c1786ffbbdd5
Computhink-CS125 <cs125@computhink.com.sg>
2025-03-01 06:55:19 +0000

    Mar 1, 2025, 6:55 AM

---
+++ CT06_07/lesson7.py
@@ -0,0 +1,60 @@
+# print("Hello from lesson 7")
+# #  Lesson 7 - For Loop (Part 2)
+
+# ## Recap 1: Debugging Average Score Program
+
+# ### There is a total of 3 bugs in the following program.
+# ### Identify and fix the bugs:
+
+# score_one = 80
+# score_two = 90
+# score_three = 75
+
+# total = score_one + score_two + score_three
+
+# average_score = total / 3
+
+# student_name = "Alex"
+
+# print("Average score for " + student_name  + " is: " + str(average_score))
+
+# print(round(average_score))
+
+
+
+
+
+
+
+# Task 1
+# for i in range(1, 11):
+#     print(i)
+
+
+# Task 2
+# for i in range(2, 21, 2):
+#     print(i)
+
+
+# Task 3
+# for i in range(10, 0, -1):
+#     print(i)
+
+
+
+
+
+
+
+
+# word = input("What word would you like to repeat?")
+
+# Repetitivenumber = int(input("How many times would you like it to repeat?"))
+
+# for i in range(Repetitivenumber, ):
+    # print(word)
+
+
+
+
+name = input("Whatingtons is your name sir?")
