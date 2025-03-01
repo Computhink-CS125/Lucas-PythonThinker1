d4eda638d73068cf899285ed1ddd30308d81d2f2
Computhink-CS125 <cs125@computhink.com.sg>
2025-03-01 07:24:51 +0000

    Mar 1, 2025, 7:24 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -80,4 +80,4 @@
 TimeTableNumbure = input("What number would you like to see the timetable of? ")
 
 for i in range(1 , 13):
-    print(str(TimeTableNumbure) + " x " + str(i))
+    print(str(TimeTableNumbure) + " x " + str(i) + "+")
