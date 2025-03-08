fd66be74daed99b64b81cc453941509f271ad242
Computhink-CS125 <cs125@computhink.com.sg>
2025-03-08 06:41:13 +0000

    Mar 8, 2025, 6:41 AM

--- Test.py
+++ Test.py
@@ -9,7 +9,7 @@
 
 start = int(input("What number would you like to start with? ")) # Asking the user what number would the user wants to start with.
 end = int(input("What number would you like to end with? ")) # Asking the user what number the user wants to end with.
-increment = int(input("What number would you like to be an increment of? ")) # Asking the user what number can the increment 
+increment = int(input("What number would you like to be an increment of? ")) # Asking the user what number can the increment be.
 
-for i in range(start, end + 1, increment):
+for i in range(start, end + 1, increment): 
     print(i)
