7a8de9056db80f3bc8026722b190502018ebde8c
Computhink-CS125 <cs125@computhink.com.sg>
2025-03-08 06:29:16 +0000

    Mar 8, 2025, 6:29 AM

--- Test.py
+++ Test.py
@@ -2,6 +2,6 @@
 # Write a PYTHON program that asks for user's name. The program should generate a greeting message mentioning the user's name.
 
 
-name = input("What is your name? ") # 
+name = input("What is your name? ") # Asking the 
 
 print("Nice to meet you, " + name + "!")
