a3814c596281d07b5af3d9277b9aafbbe959961b
Computhink-CS125 <cs125@computhink.com.sg>
2025-03-08 06:36:12 +0000

    Mar 8, 2025, 6:36 AM

--- Test.py
+++ Test.py
@@ -9,4 +9,4 @@
 
 start = input("What number would you like to start with? ")
 end = input("What number would you like to end with? ")
-increment = input("What number would you like to be an inc")
+increment = input("What number would you like to ")
