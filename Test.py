9535750aa62e9ff24f018f1ee106fa3c060f5ddb
Computhink-CS125 <cs125@computhink.com.sg>
2025-03-08 07:07:03 +0000

    Mar 8, 2025, 7:07 AM

--- Test.py
+++ Test.py
@@ -1,68 +1,71 @@
-# # # # # # # # # # # Test.py
-# # # # # # # # # # # Write a PYTHON program that asks for user's name. The program should generate a greeting message mentioning the user's name.
+# # # # # # # # # # # # Test.py
+# # # # # # # # # # # # Write a PYTHON program that asks for user's name. The program should generate a greeting message mentioning the user's name.
 
 
-# # # # # # # # # # name = input("What is your name? ") # Asking the user his/her name
+# # # # # # # # # # # name = input("What is your name? ") # Asking the user his/her name
 
-# # # # # # # # # # print("Nice to meet you, " + name + "!") # The output of what it is supposed to print
-
-
-# # # # # # # # # start = int(input("What number would you like to start with? ")) # Asking the user what number would the user wants to start with.
-# # # # # # # # # end = int(input("What number would you like to end with? ")) # Asking the user what number the user wants to end with.
-# # # # # # # # # increment = int(input("What number would you like to be an increment of? ")) # Asking the user what number can the increment be.
-
-# # # # # # # # # for i in range(start, end, increment): # counting from the start to end with the increment of the user's selected number.
-# # # # # # # # #     print(i) # The output of what it is supposed to print 
-# # # # # # # # age = input("What is your age")
-# # # # # # # # futureAge = age + 2
-# # # # # # # # print(futureAge)
-
-# # # # # # # x = int(input("First number? "))
-# # # # # # # y = int(input("Second number? "))
-# # # # # # # results = x + y / 2
-# # # # # # # print(results)
-
-# # # # # # batman = "10"
-# # # # # # robin = "2"
-# # # # # # print(batman + robin)
+# # # # # # # # # # # print("Nice to meet you, " + name + "!") # The output of what it is supposed to print
 
 
-# # # # # age = 11
-# # # # # name = "John"
+# # # # # # # # # # start = int(input("What number would you like to start with? ")) # Asking the user what number would the user wants to start with.
+# # # # # # # # # # end = int(input("What number would you like to end with? ")) # Asking the user what number the user wants to end with.
+# # # # # # # # # # increment = int(input("What number would you like to be an increment of? ")) # Asking the user what number can the increment be.
 
-# # # # # print(age + name)
-# # # # for count in range(100, 0, -1):
-# # # # 	print(count)
+# # # # # # # # # # for i in range(start, end, increment): # counting from the start to end with the increment of the user's selected number.
+# # # # # # # # # #     print(i) # The output of what it is supposed to print 
+# # # # # # # # # age = input("What is your age")
+# # # # # # # # # futureAge = age + 2
+# # # # # # # # # print(futureAge)
+
+# # # # # # # # x = int(input("First number? "))
+# # # # # # # # y = int(input("Second number? "))
+# # # # # # # # results = x + y / 2
+# # # # # # # # print(results)
+
+# # # # # # # batman = "10"
+# # # # # # # robin = "2"
+# # # # # # # print(batman + robin)
 
 
+# # # # # # age = 11
+# # # # # # name = "John"
+
+# # # # # # print(age + name)
+# # # # # for count in range(100, 0, -1):
+# # # # # 	print(count)
+
+
+
+
+
+# # # # name = "DENNIS"
+# # # # for letter in name:
+# # # # 	print(letter)
+
+
+
+
+
+
+# # # x = '1'
+# # # b = '1'
+# # # c = x + b
 
 
 
 # # # name = "DENNIS"
-# # # for letter in name:
+# # # for letter in range(len(name)):
 # # # 	print(letter)
+# # x = int(input("First number? "))
+# # y = int(input("Second number? "))
+# # results = x + y / 2
+# # print(results)
 
 
+# age = 11
+# name = "John"
 
-
-
-
-# # x = '1'
-# # b = '1'
-# # c = x + b
-
-
-
-# # name = "DENNIS"
-# # for letter in range(len(name)):
-# # 	print(letter)
-# x = int(input("First number? "))
-# y = int(input("Second number? "))
-# results = x + y / 2
-# print(results)
-
-
-age = 11
-name = "John"
-
-print(age + name)
+# print(age + name)
+name = "DENNIS"
+for letter in name:
+	print(letter)
