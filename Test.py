bc5746b3cac3d9c63417b26a298e8cc73ef36a7b
Computhink-CS125 <cs125@computhink.com.sg>
2025-03-08 06:49:50 +0000

    Mar 8, 2025, 6:49 AM

--- Test.py
+++ Test.py
@@ -1,18 +1,23 @@
-# # # Test.py
-# # # Write a PYTHON program that asks for user's name. The program should generate a greeting message mentioning the user's name.
+# # # # Test.py
+# # # # Write a PYTHON program that asks for user's name. The program should generate a greeting message mentioning the user's name.
 
 
-# # name = input("What is your name? ") # Asking the user his/her name
+# # # name = input("What is your name? ") # Asking the user his/her name
 
-# # print("Nice to meet you, " + name + "!") # The output of what it is supposed to print
+# # # print("Nice to meet you, " + name + "!") # The output of what it is supposed to print
 
 
-# start = int(input("What number would you like to start with? ")) # Asking the user what number would the user wants to start with.
-# end = int(input("What number would you like to end with? ")) # Asking the user what number the user wants to end with.
-# increment = int(input("What number would you like to be an increment of? ")) # Asking the user what number can the increment be.
+# # start = int(input("What number would you like to start with? ")) # Asking the user what number would the user wants to start with.
+# # end = int(input("What number would you like to end with? ")) # Asking the user what number the user wants to end with.
+# # increment = int(input("What number would you like to be an increment of? ")) # Asking the user what number can the increment be.
 
-# for i in range(start, end, increment): # counting from the start to end with the increment of the user's selected number.
-#     print(i) # The output of what it is supposed to print 
-age = input("What is your age")
-futureAge = age + 2
-print(futureAge)
+# # for i in range(start, end, increment): # counting from the start to end with the increment of the user's selected number.
+# #     print(i) # The output of what it is supposed to print 
+# age = input("What is your age")
+# futureAge = age + 2
+# print(futureAge)
+
+x = int(input("First number? "))
+y = int(input("Second number? "))
+results = x + y / 2
+print(results)
