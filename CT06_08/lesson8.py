37bf32a0e885ba8f7f16ec2727a42c18903988b8
Computhink-CS125 <cs125@computhink.com.sg>
2025-03-15 07:24:20 +0000

    Mar 15, 2025, 7:24 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,27 +1,30 @@
-# # import time
-# # numberForCountdownAsker = int(input("What number would you like to count down from per second? "))
-# # for i in range(numberForCountdownAsker, 0, -1):
-# #     print(i)
-# #     time.sleep(1)
+# # # import time
+# # # numberForCountdownAsker = int(input("What number would you like to count down from per second? "))
+# # # for i in range(numberForCountdownAsker, 0, -1):
+# # #     print(i)
+# # #     time.sleep(1)
 
 
 
 
-# import random
+# # import random
 
-# num = random.randint(1, 6)
+# # num = random.randint(1, 6)
 
-# print(num)
+# # print(num)
 
 
 
-x = True
-print(x)
+# x = True
+# print(x)
 
-var1 = True
-var2 = True
-print(var1 == var2)
+# var1 = True
+# var2 = True
+# print(var1 == var2)
 
-var1 = True
-var2 = False
-print(var1 == var2)
+# var1 = True
+# var2 = False
+# print(var1 == var2)
+
+
+num1 = random.randit
