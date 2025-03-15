abee4efc973e0fde6ad1cb6b052834951c78c2b7
Computhink-CS125 <cs125@computhink.com.sg>
2025-03-15 06:28:43 +0000

    Mar 15, 2025, 6:28 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -145,7 +145,7 @@
 sum  = 0
 
 for i in range(1, 6):
-    sum = sum + int(input("What is the score of student #" + num + "" ))
+    sum = sum + int(input("What is the score of student #" + num + "?" ))
 
     average = sum / 5
 
