9aab8066e934a4a09dedc903d30d13cdd5f3682c
Computhink-CS125 <cs125@computhink.com.sg>
2025-03-15 06:22:41 +0000

    Mar 15, 2025, 6:22 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,24 +1,24 @@
-# # print("Hello from lesson 7")
-# # #  Lesson 7 - For Loop (Part 2)
+# # # print("Hello from lesson 7")
+# # # #  Lesson 7 - For Loop (Part 2)
 
-# # ## Recap 1: Debugging Average Score Program
+# # # ## Recap 1: Debugging Average Score Program
 
-# # ### There is a total of 3 bugs in the following program.
-# # ### Identify and fix the bugs:
+# # # ### There is a total of 3 bugs in the following program.
+# # # ### Identify and fix the bugs:
 
-# # score_one = 80
-# # score_two = 90
-# # score_three = 75
+# # # score_one = 80
+# # # score_two = 90
+# # # score_three = 75
 
-# # total = score_one + score_two + score_three
+# # # total = score_one + score_two + score_three
 
-# # average_score = total / 3
+# # # average_score = total / 3
 
-# # student_name = "Alex"
+# # # student_name = "Alex"
 
-# # print("Average score for " + student_name  + " is: " + str(average_score))
+# # # print("Average score for " + student_name  + " is: " + str(average_score))
 
-# # print(round(average_score))
+# # # print(round(average_score))
 
 
 
@@ -26,19 +26,19 @@
 
 
 
-# # Task 1
-# # for i in range(1, 11):
-# #     print(i)
+# # # Task 1
+# # # for i in range(1, 11):
+# # #     print(i)
 
 
-# # Task 2
-# # for i in range(2, 21, 2):
-# #     print(i)
+# # # Task 2
+# # # for i in range(2, 21, 2):
+# # #     print(i)
 
 
-# # Task 3
-# # for i in range(10, 0, -1):
-# #     print(i)
+# # # Task 3
+# # # for i in range(10, 0, -1):
+# # #     print(i)
 
 
 
@@ -47,39 +47,39 @@
 
 
 
-# # word = input("What word would you like to repeat?")
+# # # word = input("What word would you like to repeat?")
 
-# # Repetitivenumber = int(input("How many times would you like it to repeat?"))
+# # # Repetitivenumber = int(input("How many times would you like it to repeat?"))
 
-# # for i in range(Repetitivenumber, ):
-#     # print(word)
+# # # for i in range(Repetitivenumber, ):
+# #     # print(word)
 
 
 
 
-# # name = input("What is your name?")
-# # TheRepeatedNumber = int(input("How many times would you like to repeat it?"))
-# # 
-# # for i in range(TheRepeatedNumber,):
-#     # print(("nice to meet you, " + name) + "!")
+# # # name = input("What is your name?")
+# # # TheRepeatedNumber = int(input("How many times would you like to repeat it?"))
+# # # 
+# # # for i in range(TheRepeatedNumber,):
+# #     # print(("nice to meet you, " + name) + "!")
 
 
 
 
 
-# # sum = 0
+# # # sum = 0
 
-# # for n in range(1,6):
-# #     sum = sum + int(input("What is number " + str(n)))
+# # # for n in range(1,6):
+# # #     sum = sum + int(input("What is number " + str(n)))
 
-# # print(sum)
+# # # print(sum)
 
 
 
 
-# TimeTableNumbure = int(input("What number would you like to see the timetable of? "))
-# for i in range(1 , 13):
-#     print(str(TimeTableNumbure) + " x " + str(i) + "=" + str( i * TimeTableNumbure))
+# # TimeTableNumbure = int(input("What number would you like to see the timetable of? "))
+# # for i in range(1 , 13):
+# #     print(str(TimeTableNumbure) + " x " + str(i) + "=" + str( i * TimeTableNumbure))
 
 
 
@@ -137,7 +137,7 @@
 
 
 
-num = int(input("Give me a number to make a number pyramid: "))
+# num = int(input("Give me a number to make a number pyramid: "))
 
-for i in range(1, num +1):
-    print(str(i) * i)
+# for i in range(1, num +1):
+#     print(str(i) * i)
