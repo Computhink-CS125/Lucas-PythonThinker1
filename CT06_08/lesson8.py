48acb0e198992edefa78cc529a26f36e80b3c46e
Computhink-CS125 <cs125@computhink.com.sg>
2025-03-15 06:56:50 +0000

    Mar 15, 2025, 6:56 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,5 +1,5 @@
 import time
-numberForCountdownAsker = input("What number would you like to count down")
+numberForCountdownAsker = input("What number would you like to count down from per second? ")
 for i in range(10, 0, -1):
     print(i)
     time.sleep(1)
