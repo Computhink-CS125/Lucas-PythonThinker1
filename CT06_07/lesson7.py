8740fb64d94b6769e858f181427f2e1a08251029
Computhink-CS125 <cs125@computhink.com.sg>
2025-03-15 06:40:22 +0000

    Mar 15, 2025, 6:40 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -166,4 +166,4 @@ for i in range(1, numstudents):
 
     average = sum / 5
 
-printint(("The average score for the " + numstudents + "is " +  str(average)))
+print(int(("The average score for the " + numstudents + "is " +  str(average))))
