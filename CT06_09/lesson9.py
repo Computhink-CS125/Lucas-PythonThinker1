99be83bfe8927d43dc278b51e98ce470be784b78
Computhink-CS125 <cs125@computhink.com.sg>
2025-03-22 07:28:54 +0000

    Mar 22, 2025, 7:28 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,36 +1,40 @@
+# # import random
+
+# # num1 = random.randint(1,6)
+# # num2 = random.randint(1,6)
+# # num3 = random.randint(1,6)
+# # print("The first number : " + str(num1))
+# # print("The second number : " + str(num2))
+# # print("The third number : " + str(num3))
+# # num1_IsEven = num1 % 2 == 0
+# # num2_IsEven = num2 % 2 == 0
+# # num3_IsEven = num2 % 2 == 0
+
+# # all_even_odd = num1_IsEven == num2_IsEven
+
+# # print(all_even_odd)
+
+
+
+
+# # question = int(input("How many days have you borrowed this book?"))
+
+# # if question > 25:
+# #     # print("remember tO Return YOUr book!")
+
+
+
+
+
+
 # import random
 
-# num1 = random.randint(1,6)
-# num2 = random.randint(1,6)
-# num3 = random.randint(1,6)
-# print("The first number : " + str(num1))
-# print("The second number : " + str(num2))
-# print("The third number : " + str(num3))
-# num1_IsEven = num1 % 2 == 0
-# num2_IsEven = num2 % 2 == 0
-# num3_IsEven = num2 % 2 == 0
+# num1 = random.randint(1,10)
+# UserGuess = int(input("What is your guess number from 1 to 10?"))
 
-# all_even_odd = num1_IsEven == num2_IsEven
-
-# print(all_even_odd)
+# if guess == num1:
+#     print
 
 
 
-
-# question = int(input("How many days have you borrowed this book?"))
-
-# if question > 25:
-#     # print("remember tO Return YOUr book!")
-
-
-
-
-
-
-import random
-
-num1 = random.randint(1,10)
-UserGuess = int(input("What is your guess number from 1 to 10?"))
-
-if guess == num1:
-    print
+tot
