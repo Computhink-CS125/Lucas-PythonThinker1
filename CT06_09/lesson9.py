fa55b5d2c93c31f98dbb8037bab1140e3e7b1f9c
Computhink-CS125 <cs125@computhink.com.sg>
2025-03-22 06:49:42 +0000

    Mar 22, 2025, 6:49 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -6,4 +6,5 @@ num3 = random.randint(1,6)
 print("The first number : " + str(num1))
 print("The second number : " + str(num2))
 print("The third number : " + str(num3))
-EvenNum = num1 % 2 == 0
+EvenNum1 = num1 % 2 == 0
+EvenNum2
