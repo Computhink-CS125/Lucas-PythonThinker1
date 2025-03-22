e13e9b9d17f38d090deb0a01995effa6f10a73b5
Computhink-CS125 <cs125@computhink.com.sg>
2025-03-22 06:48:34 +0000

    Mar 22, 2025, 6:48 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,1 +1,8 @@
 import random
+
+num1 = random.randint(1,6)
+num2 = random.randint(1,6)
+num3 = random.randint(1,6)
+print("The first number : " + str(num1))
+print("The second number : " + str(num2))
+print("The third number : " + str(num3))
