6e3b24d829df6d435c4d21c30f01249920821942
Computhink-CS125 <cs125@computhink.com.sg>
2025-03-22 06:36:32 +0000

    Mar 22, 2025, 6:36 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -47,8 +47,8 @@ import random
 NoOfQuestions = int(input("How many questions do you want ME to give you?"))
 
 for i in range(NoOfQuestions):
-    num1 = random.randint()
-    num2 = random.randint()
+    num1 = random.randint(1,10)
+    num2 = random.randint(1.10)
     userAns = int(input("What is" + str(num1) + " x " + str(num2) + " ? "))
     Answer = num1 + num2
     print(userAns == Answer)
