64ce508f56da617a7629c60d3151a6fb335324ae
Computhink-CS125 <cs125@computhink.com.sg>
2025-03-22 06:51:12 +0000

    Mar 22, 2025, 6:51 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -8,4 +8,4 @@ print("The second number : " + str(num2))
 print("The third number : " + str(num3))
 num1_IsEvenNum1 = num1 % 2 == 0
 num2_IsEvenNum2 = num2 % 2 == 0
-EvenNum3 = num2 % 2 == 0
+num3_IsEvenNum3 = num2 % 2 == 0
