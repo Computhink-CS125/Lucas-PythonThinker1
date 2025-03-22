4907b6fb8f2dad19a9404c783eef437be2648a6d
Computhink-CS125 <cs125@computhink.com.sg>
2025-03-22 06:44:14 +0000

    Mar 22, 2025, 6:44 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,54 +1,54 @@
-# # # # # import time
-# # # # # numberForCountdownAsker = int(input("What number would you like to count down from per second? "))
-# # # # # for i in range(numberForCountdownAsker, 0, -1):
-# # # # #     print(i)
-# # # # #     time.sleep(1)
+# # # # # # import time
+# # # # # # numberForCountdownAsker = int(input("What number would you like to count down from per second? "))
+# # # # # # for i in range(numberForCountdownAsker, 0, -1):
+# # # # # #     print(i)
+# # # # # #     time.sleep(1)
 
 
 
 
-# # # # import random
+# # # # # import random
 
-# # # # num = random.randint(1, 6)
+# # # # # num = random.randint(1, 6)
 
-# # # # print(num)
+# # # # # print(num)
 
 
 
-# # # x = True
-# # # print(x)
+# # # # x = True
+# # # # print(x)
 
-# # # var1 = True
-# # # var2 = True
-# # # print(var1 == var2)
+# # # # var1 = True
+# # # # var2 = True
+# # # # print(var1 == var2)
 
-# # # var1 = True
-# # # var2 = False
-# # # print(var1 == var2)
+# # # # var1 = True
+# # # # var2 = False
+# # # # print(var1 == var2)
+
+# # # import random
+# # # num1 = random.randint(1, 50)
+# # # num2 = random.randint(1, 50)
+# # # answer = int(input("what is the sum of " + str(num1) + "+" + str(num2) + " ?"))
+# # # print(answer == num1 + num2)
 
 # # import random
-# # num1 = random.randint(1, 50)
-# # num2 = random.randint(1, 50)
-# # answer = int(input("what is the sum of " + str(num1) + "+" + str(num2) + " ?"))
-# # print(answer == num1 + num2)
+# # guess = int(input("What number would you like to guess? "))
+# # number = random.randint(1,10)
+
+# # print("Guess: " + str(guess))
+# # print("Num: " + str(number))
+
+# # print(guess == number)
+
 
 # import random
-# guess = int(input("What number would you like to guess? "))
-# number = random.randint(1,10)
 
-# print("Guess: " + str(guess))
-# print("Num: " + str(number))
+# NoOfQuestions = int(input("How many questions do you want ME to give you?"))
 
-# print(guess == number)
-
-
-import random
-
-NoOfQuestions = int(input("How many questions do you want ME to give you?"))
-
-for i in range(NoOfQuestions):
-    # num1 = random.randint(1,100)
-    num2 = random.randint(-1,100)
-    userAns = int(input("What is" + str(num1) + " x " + str(num2) + " ? "))
-    Answer = num1 + num2
-    print(userAns == Answer)
+# for i in range(NoOfQuestions):
+#     num1 = random.randint(1,100)
+#     num2 = random.randint(-1,100)
+#     userAns = int(input("What is" + str(num1) + " x " + str(num2) + " ? "))
+#     Answer = num1 + num2
+#     print(userAns == Answer)
