6a2d0b71809a369824e5d00fc0874dd2332b7cb1
Computhink-CS125 <cs125@computhink.com.sg>
2025-03-22 06:20:23 +0000

    Mar 22, 2025, 6:20 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,33 +1,35 @@
-# # # import time
-# # # numberForCountdownAsker = int(input("What number would you like to count down from per second? "))
-# # # for i in range(numberForCountdownAsker, 0, -1):
-# # #     print(i)
-# # #     time.sleep(1)
+# # # # import time
+# # # # numberForCountdownAsker = int(input("What number would you like to count down from per second? "))
+# # # # for i in range(numberForCountdownAsker, 0, -1):
+# # # #     print(i)
+# # # #     time.sleep(1)
 
 
 
 
-# # import random
+# # # import random
 
-# # num = random.randint(1, 6)
+# # # num = random.randint(1, 6)
 
-# # print(num)
+# # # print(num)
 
 
 
-# x = True
-# print(x)
+# # x = True
+# # print(x)
 
-# var1 = True
-# var2 = True
-# print(var1 == var2)
+# # var1 = True
+# # var2 = True
+# # print(var1 == var2)
 
-# var1 = True
-# var2 = False
-# print(var1 == var2)
+# # var1 = True
+# # var2 = False
+# # print(var1 == var2)
+
+# import random
+# num1 = random.randint(1, 50)
+# num2 = random.randint(1, 50)
+# answer = int(input("what is the sum of " + str(num1) + "+" + str(num2) + " ?"))
+# print(answer == num1 + num2)
 
 import random
-num1 = random.randint(1, 50)
-num2 = random.randint(1, 50)
-answer = int(input("what is the sum of " + str(num1) + "+" + str(num2) + " ?"))
-print(answer == num1 + num2)
