03bf604c852d1776627cd123a594d009e5504910
Computhink-CS125 <cs125@computhink.com.sg>
2025-03-22 07:13:28 +0000

    Mar 22, 2025, 7:13 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,23 +1,23 @@
-import random
+# import random
 
-num1 = random.randint(1,6)
-num2 = random.randint(1,6)
-num3 = random.randint(1,6)
-print("The first number : " + str(num1))
-print("The second number : " + str(num2))
-print("The third number : " + str(num3))
-num1_IsEven = num1 % 2 == 0
-num2_IsEven = num2 % 2 == 0
-num3_IsEven = num2 % 2 == 0
+# num1 = random.randint(1,6)
+# num2 = random.randint(1,6)
+# num3 = random.randint(1,6)
+# print("The first number : " + str(num1))
+# print("The second number : " + str(num2))
+# print("The third number : " + str(num3))
+# num1_IsEven = num1 % 2 == 0
+# num2_IsEven = num2 % 2 == 0
+# num3_IsEven = num2 % 2 == 0
 
-all_even_odd = num1_IsEven == num2_IsEven
+# all_even_odd = num1_IsEven == num2_IsEven
 
-print(all_even_odd)
-
-
+# print(all_even_odd)
 
 
-question = int(input("How many days have you borrowed this book?"))
 
-if question > 25:
-    print("remember tO Return YOUr bOok!")
+
+# question = int(input("How many days have you borrowed this book?"))
+
+# if question > 25:
+#     # print("remember tO Return YOUr book!")
