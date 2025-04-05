b6eab3f962082b2dc0187cfa28488f877f173be4
chialucas <chialucas56@gmail.com>
2025-04-05 06:49:42 +0000

    Apr 5, 2025, 6:49 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,39 +1,41 @@
-# # # sigma boyyy
+# # # # sigma boyyy
 
-# # PriceOItem = int(input("What is the price of the item you wish to buy?"))
+# # # PriceOItem = int(input("What is the price of the item you wish to buy?"))
 
-# # if PriceOItem <= 5:
-# #     print("Sounds Cheap you brokey!")
-# # elif PriceOItem <= 50:
-# #     print("Aight, go ahead buddy boy")
-# # elif PriceOItem <= 500:
-# #     print("Alright I think thats enough...")
-# # elif PriceOItem <= 501:
-# #     print("Seriously? 501?")
-# # elif PriceOItem <= 502:
-# #     print("Ok bro, 502?")
-# # else: print("OK THATS ENOUGH, I KNOW YOU WANT TO SPEEDRUN POVERTY BUT THATS ENOUGH OKAY! CHILL!!!")
-
-
-
-
-# rider1 = 125
-# rider2 = 150
-
-# result = rider1 > 120 and rider2 > 120
-# print (result)
-
-# num = int(input("What Number would you input"))
-
-# if num % 3 == 0 and num % 7 == 0:
-#     print("The number can be divisable by 3 and 7")
+# # # if PriceOItem <= 5:
+# # #     print("Sounds Cheap you brokey!")
+# # # elif PriceOItem <= 50:
+# # #     print("Aight, go ahead buddy boy")
+# # # elif PriceOItem <= 500:
+# # #     print("Alright I think thats enough...")
+# # # elif PriceOItem <= 501:
+# # #     print("Seriously? 501?")
+# # # elif PriceOItem <= 502:
+# # #     print("Ok bro, 502?")
+# # # else: print("OK THATS ENOUGH, I KNOW YOU WANT TO SPEEDRUN POVERTY BUT THATS ENOUGH OKAY! CHILL!!!")
 
 
 
 
+# # rider1 = 125
+# # rider2 = 150
 
-rider1 = 25
-rider2 = 6
+# # result = rider1 > 120 and rider2 > 120
+# # print (result)
 
-result = rider1 >= 18 or rider2 >= 18
-print(result)
+# # num = int(input("What Number would you input"))
+
+# # if num % 3 == 0 and num % 7 == 0:
+# #     print("The number can be divisable by 3 and 7")
+
+
+
+
+
+# rider1 = 25
+# rider2 = 6
+
+# result = rider1 >= 18 or rider2 >= 18
+# print(result)
+
+
