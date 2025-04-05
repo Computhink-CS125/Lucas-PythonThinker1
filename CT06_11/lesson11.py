287fe4523c5c853b2c175f6f216c50ce9ff52e5d
chialucas <chialucas56@gmail.com>
2025-04-05 06:33:39 +0000

    Apr 5, 2025, 6:33 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,24 +1,26 @@
-# # sigma boyyy
+# # # sigma boyyy
 
-# PriceOItem = int(input("What is the price of the item you wish to buy?"))
+# # PriceOItem = int(input("What is the price of the item you wish to buy?"))
 
-# if PriceOItem <= 5:
-#     print("Sounds Cheap you brokey!")
-# elif PriceOItem <= 50:
-#     print("Aight, go ahead buddy boy")
-# elif PriceOItem <= 500:
-#     print("Alright I think thats enough...")
-# elif PriceOItem <= 501:
-#     print("Seriously? 501?")
-# elif PriceOItem <= 502:
-#     print("Ok bro, 502?")
-# else: print("OK THATS ENOUGH, I KNOW YOU WANT TO SPEEDRUN POVERTY BUT THATS ENOUGH OKAY! CHILL!!!")
-
-
+# # if PriceOItem <= 5:
+# #     print("Sounds Cheap you brokey!")
+# # elif PriceOItem <= 50:
+# #     print("Aight, go ahead buddy boy")
+# # elif PriceOItem <= 500:
+# #     print("Alright I think thats enough...")
+# # elif PriceOItem <= 501:
+# #     print("Seriously? 501?")
+# # elif PriceOItem <= 502:
+# #     print("Ok bro, 502?")
+# # else: print("OK THATS ENOUGH, I KNOW YOU WANT TO SPEEDRUN POVERTY BUT THATS ENOUGH OKAY! CHILL!!!")
 
 
-rider1 = 125
-rider2 = 150
 
-result = rider1 > 120 and rider2 > 120
-print (result)
+
+# rider1 = 125
+# rider2 = 150
+
+# result = rider1 > 120 and rider2 > 120
+# print (result)
+
+num = 
