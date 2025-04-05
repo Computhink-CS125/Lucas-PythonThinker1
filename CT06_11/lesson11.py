059f929f43c102cbcb63ee26e9fd2c7ebc8fc33a
chialucas <chialucas56@gmail.com>
2025-04-05 06:25:48 +0000

    Apr 5, 2025, 6:25 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,15 +1,15 @@
-# sigma boyyy
+# # sigma boyyy
 
-PriceOItem = int(input("What is the price of the item you wish to buy?"))
+# PriceOItem = int(input("What is the price of the item you wish to buy?"))
 
-if PriceOItem <= 5:
-    print("Sounds Cheap you brokey!")
-elif PriceOItem <= 50:
-    print("Aight, go ahead buddy boy")
-elif PriceOItem <= 500:
-    print("Alright I think thats enough...")
-elif PriceOItem <= 501:
-    print("Seriously? 501?")
-elif PriceOItem <= 502:
-    print("Ok bro, 502?")
-else: print("OK THATS ENOUGH, I KNOW YOU WANT TO SPEEDRUN POVERTY BUT THATS ENOUGH OKAY! CHILL!!!")
+# if PriceOItem <= 5:
+#     print("Sounds Cheap you brokey!")
+# elif PriceOItem <= 50:
+#     print("Aight, go ahead buddy boy")
+# elif PriceOItem <= 500:
+#     print("Alright I think thats enough...")
+# elif PriceOItem <= 501:
+#     print("Seriously? 501?")
+# elif PriceOItem <= 502:
+#     print("Ok bro, 502?")
+# else: print("OK THATS ENOUGH, I KNOW YOU WANT TO SPEEDRUN POVERTY BUT THATS ENOUGH OKAY! CHILL!!!")
