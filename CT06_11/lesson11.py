a5b9fb6fffe4d58f2e1c47c667f76aaffed14740
chialucas <chialucas56@gmail.com>
2025-04-05 07:22:44 +0000

    Apr 5, 2025, 7:22 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,62 +1,64 @@
-# # # # # # sigma boyyy
+# # # # # # # sigma boyyy
 
-# # # # # PriceOItem = int(input("What is the price of the item you wish to buy?"))
+# # # # # # PriceOItem = int(input("What is the price of the item you wish to buy?"))
 
-# # # # # if PriceOItem <= 5:
-# # # # #     print("Sounds Cheap you brokey!")
-# # # # # elif PriceOItem <= 50:
-# # # # #     print("Aight, go ahead buddy boy")
-# # # # # elif PriceOItem <= 500:
-# # # # #     print("Alright I think thats enough...")
-# # # # # elif PriceOItem <= 501:
-# # # # #     print("Seriously? 501?")
-# # # # # elif PriceOItem <= 502:
-# # # # #     print("Ok bro, 502?")
-# # # # # else: print("OK THATS ENOUGH, I KNOW YOU WANT TO SPEEDRUN POVERTY BUT THATS ENOUGH OKAY! CHILL!!!")
-
-
-
-
-# # # # rider1 = 125
-# # # # rider2 = 150
-
-# # # # result = rider1 > 120 and rider2 > 120
-# # # # print (result)
-
-# # # # num = int(input("What Number would you input"))
-
-# # # # if num % 3 == 0 and num % 7 == 0:
-# # # #     print("The number can be divisable by 3 and 7")
+# # # # # # if PriceOItem <= 5:
+# # # # # #     print("Sounds Cheap you brokey!")
+# # # # # # elif PriceOItem <= 50:
+# # # # # #     print("Aight, go ahead buddy boy")
+# # # # # # elif PriceOItem <= 500:
+# # # # # #     print("Alright I think thats enough...")
+# # # # # # elif PriceOItem <= 501:
+# # # # # #     print("Seriously? 501?")
+# # # # # # elif PriceOItem <= 502:
+# # # # # #     print("Ok bro, 502?")
+# # # # # # else: print("OK THATS ENOUGH, I KNOW YOU WANT TO SPEEDRUN POVERTY BUT THATS ENOUGH OKAY! CHILL!!!")
 
 
 
 
+# # # # # rider1 = 125
+# # # # # rider2 = 150
 
-# # # rider1 = 25
-# # # rider2 = 6
+# # # # # result = rider1 > 120 and rider2 > 120
+# # # # # print (result)
 
-# # # result = rider1 >= 18 or rider2 >= 18
-# # # print(result)
+# # # # # num = int(input("What Number would you input"))
+
+# # # # # if num % 3 == 0 and num % 7 == 0:
+# # # # #     print("The number can be divisable by 3 and 7")
 
 
-# # user1 = int(input("SIRINGTONS HOW OLD R UUUU =}"))
+
+
+
+# # # # rider1 = 25
+# # # # rider2 = 6
+
+# # # # result = rider1 >= 18 or rider2 >= 18
+# # # # print(result)
+
+
+# # # user1 = int(input("SIRINGTONS HOW OLD R UUUU =}"))
     
-# # if user1 <= 18:
-# #     print("OKE u get free dicount!")
+# # # if user1 <= 18:
+# # #     print("OKE u get free dicount!")
 
 
 
 
-# password = input("What is da SECERETOOLOO PASSYWORD :))")
+# # password = input("What is da SECERETOOLOO PASSYWORD :))")
 
-# if not password == pithon1twothree:
-#     print("HAHAH U HACKUR U NEBER GET FWEE PASSYWORD EHEHEHA")
-# else:
-#     print("Ight! Welcome back!")
+# # if not password == pithon1twothree:
+# #     print("HAHAH U HACKUR U NEBER GET FWEE PASSYWORD EHEHEHA")
+# # else:
+# #     print("Ight! Welcome back!")
 
-burg = input("Do you want a burger🍔 :)")
-fries = input("Want some fries?🍟")
-drink= input("Saving your thirst for some drinks?🍷")
+# burg = input("Do you want a burger🍔 :)")
+# fries = input("Want some fries?🍟")
+# drink= input("Saving your thirst for some drinks?🍷")
 
-if burg == "Yes" and fries == "Yes" and not drink == "Yes":
-    print("So... you're saving your thirst for something else?")
+# if burg == "Yes" and fries == "Yes" and not drink == "Yes":
+#     print("So... you're saving your thirst for something else?")
+
+
