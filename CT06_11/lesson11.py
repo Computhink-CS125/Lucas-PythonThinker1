14a56f37edf9496a47f99b2265a6ce3a5f841a77
chialucas <chialucas56@gmail.com>
2025-04-05 06:13:31 +0000

    Apr 5, 2025, 6:13 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -3,4 +3,6 @@
 PriceOItem = int(input("What is the price of the item you wish to buy?"))
 
 if PriceOItem <= 5:
-    print("Sounds Cheap you brokey")
+    print("Sounds Cheap you brokey!")
+elif PriceOItem <= 50:
+    print("")
