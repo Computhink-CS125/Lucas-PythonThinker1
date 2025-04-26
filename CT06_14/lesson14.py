82062a3a7714773af56156b7817ae5dbdead5832
chialucas <chialucas56@gmail.com>
2025-04-26 06:46:10 +0000

    Apr 26, 2025, 6:46 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -11,8 +11,8 @@ for i in range(len(Order)):
     print(str(i+1) + ". " + Order[i])
 
 while True:
-    user = input("Please choose your pizza topping by number:")
-    if user == "end" :
+    user_input = input("Please choose your pizza topping by number:")
+    if user_input == "end" :
         break
     else:
         user.append
