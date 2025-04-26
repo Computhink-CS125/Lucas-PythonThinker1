80d6d4278d9cbf14d21a7481a19c4c73803d88f0
chialucas <chialucas56@gmail.com>
2025-04-26 05:58:00 +0000

    Apr 26, 2025, 5:58 AM

---
+++ CT06_13/lesson13.py
@@ -0,0 +1,212 @@
+# # # Balance = 1000
+
+# # # while True:
+# # #     print("Scenario:(BANK) Choose the following options: ")
+# # #     print("1. Withdraw")
+# # #     print("2. Deposit")
+# # #     print("3. Check Balance")
+# # #     print("4. Borrow money")
+# # #     print("5. Exit")
+# # #     Userchoice = int(input("Select the following by typing the number:   "))
+
+# # #     if Userchoice == 1:
+# # #         amountOwithdraw = int(input("What is the selected amount of money"))
+
+
+# # # # 1. The user starts with an account balance of $1000
+# # # # account_balance = 1000
+
+# # # # # 2. Using a 'while' loop, the program should repeatedly ask the user
+# # # # #    to choose between "Withdraw", "Deposit", "Check Balance", and
+# # # # #    "Exit" options.
+# # # # while True:
+# # # #     print("Choose between the four options below using the option number...")
+# # # #     print("1. Withdraw")
+# # # #     print("2. Deposit")
+# # # #     print("3. Check Balance")
+# # # #     print("4. Exit")
+
+# # # #     userChoice = int(input("Your choice: "))
+# # # #     # 3. If the user chooses "Withdraw":
+# # # # #     a.  Ask the user for the amount to withdraw.
+# # # # #     b.  Check if the balance is sufficient. If it is, deduct the
+# # # # #         amount from the balance and display a success message along
+# # # # #         with the remaining balance.
+# # # # #     c.  If the balance is not sufficient, display an error message.
+# # # #     if userChoice == 1:
+# # # #         amount = int(input("How much do you want to withdraw? "))
+        
+# # # #         if amount <= account_balance:
+# # # #             account_balance -= amount
+# # # #             print("You have successfully withdrew the amount.")
+# # # #             print("The current balance is: $" + str(account_balance))
+# # # #         else:
+# # # #             print("Error: Declined. Not enough money!")
+
+# # # #     if userChoice == 2:
+# # # #     # 4. If the user chooses "Deposit":
+# # # #     #     a.  Ask for the amount to deposit.
+# # # #     #     b.  Add the amount to the balance and display the updated balance.
+# # # #         amount = int(input("How much money do you want to deposit?: "))
+# # # #         # Assume number is positive
+# # # #         account_balance += amount
+# # # #         print("Success! The new account balance is: $" + str(account_balance))
+
+# # # #     if userChoice == 3:
+# # # #     # 5. If the user chooses "Check Balance":
+# # # #     #     a.  Display the current balance.
+# # # #         print("The current balance is: $" + str(account_balance))
+
+# # # #     if userChoice == 4:
+# # # #         # 6. The program should continue running (asking these options) until
+# # # #         #    the user chooses the "Exit" option.
+# # # #         break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # groceries = [
+# #     "apples",
+# #     "bread",
+# #     "carrots"
+# #     "dates"
+# #     "eggs"
+# #     "flour"
+# #     "grapes"
+# #     "honey"
+# #     "chimpanzini bananannini waw waw waw :)))"
+# # ]
+
+# # print(groceries)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# groceries = [ 
+#     "apples"
+#     "orange"
+#     "peanuts"
+# ]
+
+
+
+
+
+# print("chimpanzini banananini is my idol")
+
+
+
+
+#  # chimpanzini banananini 
+#  #chimpanzini banananini 
+#  #chimpanzini banananini 
+#  #chimpanzini banananini 
+
+groceries.append("Ice")
+
+groceries = [
+    "water"
+    "stress balls"
+    "17 robux giftcards worth each $75"
+    "a fire extinguisher"
+    "a trash can"
+    "a crate with 20 galvanised ecofriendly steel"
+    "a water bottle"
+    "hydrogen"
+    "corn"
+    "rice"
+]
+
+
+
+
+
+
+groceries = [
+    "apples"
+    "lemons"
+    "orange"
+    "teaspoon of flour"
+    "sugar"
+    "butter"
+    ""
+    ""
+]
+
+# method 1: del(groceries[2])
+
+# method 2
+# item_that_was_removed = groceries.pop(2)
+
+
+
+
+
+
+
+
