7430406bfdb1cd8ae2f4962a7a7cc9e906186b8e
chialucas <chialucas56@gmail.com>
2025-04-26 06:51:06 +0000

    Apr 26, 2025, 6:51 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -1,24 +1,24 @@
-Order = [
-"Mushroom 🤮",
-"pepperoni😁",
-"Cheese😍"
-"Extra cheese 😍😍😍"]
+# Order = [
+# "Mushroom 🤮",
+# "pepperoni😁",
+# "Cheese😍"
+# "Extra cheese 😍😍😍"]
 
-User = []
-print("The Availiable toppings are:")
+# User = []
+# print("The Availiable toppings are:")
 
-for i in range(len(Order)):
-    print(str(i+1) + ". " + Order[i])
+# for i in range(len(Order)):
+#     print(str(i+1) + ". " + Order[i])
 
-while True:
-    user_input = input("Please choose your pizza topping by number:")
-    if user_input == "end" :
-        break
-    else:
-        User.append(Order[int(user_input) - 1])
+# while True:
+#     user_input = input("Please choose your pizza topping by number:")
+#     if user_input == "end" :
+#         break
+#     else:
+#         User.append(Order[int(user_input) - 1])
         
-for i in user_input:
-    print(i)
+# for i in user_input:
+#     print(i)
 
 
 
