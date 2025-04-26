9f7b8592a4917372e72af6d744ae84da08bb8408
chialucas <chialucas56@gmail.com>
2025-04-26 07:16:08 +0000

    Apr 26, 2025, 7:16 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -36,15 +36,3 @@ t = turtle.Turtle
 t.shape("turtle")
 
 window.mainloop()
-
-import turtle 
-
-window = turtle.Screen()
-
-window.setup(width=600, height=400)
-
-t = turtle.Turtle 
-
-t.shape("turtle")
-
-window.mainloop()
