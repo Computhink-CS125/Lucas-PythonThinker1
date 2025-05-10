4ad7c7120c08624651333de84aba358d17992ed1
chialucas <chialucas56@gmail.com>
2025-05-10 06:49:06 +0000

    May 10, 2025, 6:49 AM

--- Test.py
+++ Test.py
@@ -312,12 +312,12 @@ planets = ["mercury","venus", "earth", "mars", "jupiter", "saturn", "uranus" ]
 # Do the following operations to the list provided below
 # Write the code below each question.
 
-planets = ["mercury","venus", "earth", "mars", "jupiter", "saturn", "uranus" ]
+
 
 # 1. Write code below to print the 3rd item 
 #    in this list using index e.g. earth
 
-
+print(planets[2])
 # 2. Write code to append neptune to this list.
 
 
