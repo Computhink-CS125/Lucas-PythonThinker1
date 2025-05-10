a4092eb3861382e50f72974e33e7f7d011f16646
chialucas <chialucas56@gmail.com>
2025-05-10 06:58:11 +0000

    May 10, 2025, 6:58 AM

--- Test.py
+++ Test.py
@@ -321,6 +321,7 @@ print(planets[2])
 # 2. Write code to append neptune to this list.
 
 planets.append = ("neptune")
+print
 
 # 3. Elon Musk has conquered Mars. 
 #    Rename Mars in the list to be "muskworld"
