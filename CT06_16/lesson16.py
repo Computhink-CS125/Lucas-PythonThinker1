1df4b680a247bff1d51925d835b618878a6e4cf5
chialucas <chialucas56@gmail.com>
2025-05-24 06:20:59 +0000

    May 24, 2025, 6:20 AM

--- CT06_16/lesson16.py
+++ CT06_16/lesson16.py
@@ -1,5 +1,12 @@
-import turtle
-window = turtle.Screen()
-t = turtle.Turtle()
+# import turtle
+# window = turtle.Screen()
+# t = turtle.Turtle()
 
-window.mainloop()
+# window.mainloop()
+
+
+
+
+def drawShape(length, num_sides):
+    for i in range(num_sides):
+        t.forward
