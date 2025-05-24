13b2db06569395bf4c37d3433aec13bf4589ebb9
chialucas <chialucas56@gmail.com>
2025-05-24 06:07:52 +0000

    May 24, 2025, 6:07 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -1,39 +1,108 @@
-# Order = [
-# "Mushroom 🤮",
-# "pepperoni😁",
-# "Cheese😍"
-# "Extra cheese 😍😍😍"]
+# # # # # Order = [
+# # # # # "Mushroom 🤮",
+# # # # # "pepperoni😁",
+# # # # # "Cheese😍"
+# # # # # "Extra cheese 😍😍😍"]
 
-# User = []
-# print("The Availiable toppings are:")
+# # # # # User = []
+# # # # # print("The Availiable toppings are:")
 
-# for i in range(len(Order)):
-#     print(str(i+1) + ". " + Order[i])
+# # # # # for i in range(len(Order)):
+# # # # #     print(str(i+1) + ". " + Order[i])
 
-# while True:
-#     user_input = input("Please choose your pizza topping by number:")
-#     if user_input == "end" :
-#         break
-#     else:
-#         User.append(Order[int(user_input) - 1])
+# # # # # while True:
+# # # # #     user_input = input("Please choose your pizza topping by number:")
+# # # # #     if user_input == "end" :
+# # # # #         break
+# # # # #     else:
+# # # # #         User.append(Order[int(user_input) - 1])
         
-# for i in user_input:
-#     print(i)
+# # # # # for i in user_input:
+# # # # #     print(i)
 
 
 
 
 
 
-import turtle 
+# # # # import turtle 
 
-window = turtle.Screen()
+# # # # window = turtle.Screen()
 
-window.setup(width=600, height=400)
+# # # # window.setup(width=600, height=400)
 
-t = turtle.Turtle 
+# # # # t = turtle.Turtle 
 
-t.shape("turtle")
+# # # # t.shape("turtle")
 
-window.mainloop()
+# # # # window.mainloop()
 
+# # # counter = 0
+
+# # # def increment_counter ():
+# # #     global counter
+# # #     counter += 1
+
+# # # increment_counter()
+# # # increment_counter()
+# # # increment_counter()
+# # # increment_counter()
+# # # increment_counter()
+# # # increment_counter()
+# # # increment_counter()
+# # # increment_counter()
+# # # increment_counter()
+# # # increment_counter()
+# # # increment_counter()
+# # # increment_counter()
+# # # increment_counter()
+# # # increment_counter()
+# # # increment_counter()
+# # # increment_counter()
+# # # increment_counter()
+# # # increment_counter()
+# # # increment_counter()
+# # # increment_counter()
+# # # increment_counter()
+# # # increment_counter()
+# # # increment_counter()
+# # # increment_counter()
+# # # increment_counter()
+# # # increment_counter()
+# # # increment_counter()
+# # # increment_counter()
+
+# # # print(counter)
+
+
+
+
+
+
+# # def IsEven (num):
+# #     return num % 2 == 0
+
+# # numbers = [1,2,3,4,5,6,7,8,9]
+
+# # for n in numbers:
+# #     if IsEven(n):
+# #         print(str(n) + " is even")
+# #     else:
+# #         print(str(n) + " is odd")
+
+
+
+
+
+
+
+
+def square(num):
+    return num*num
+
+print(square((5)))
+
+def sum_of_squares(num1, num2):
+    return square(num1) + square(num2)
+
+print(sum_of_squares)
