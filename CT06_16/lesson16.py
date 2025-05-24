f53d7a9e19980dd11d6019b9eabce88f71140610
chialucas <chialucas56@gmail.com>
2025-05-24 06:22:50 +0000

    May 24, 2025, 6:22 AM

--- CT06_16/lesson16.py
+++ CT06_16/lesson16.py
@@ -6,7 +6,7 @@ t = turtle.Turtle()
 def drawShape(length, num_sides):
     for i in range(num_sides):
         t.forward(length)
-        # turning t.left(360 / num_sides)
+        t.left(360 / num_sides)
 
 length_var =int(input("what is the length "))
 drawShape(length_var, 4)
