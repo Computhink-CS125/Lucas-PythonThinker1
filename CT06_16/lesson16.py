ab39622d78d862ebc8af8655d9749e2318d37a8e
chialucas <chialucas56@gmail.com>
2025-05-24 06:21:44 +0000

    May 24, 2025, 6:21 AM

--- CT06_16/lesson16.py
+++ CT06_16/lesson16.py
@@ -1,14 +1,13 @@
-# import turtle
-# window = turtle.Screen()
-# t = turtle.Turtle()
-
-# window.mainloop()
-
-
+import turtle
+window = turtle.Screen()
+t = turtle.Turtle()
 
 
 def drawShape(length, num_sides):
     for i in range(num_sides):
         t.forward(length)
 
-length_var =int(input("what"))
+length_var =int(input("what is the length "))
+
+
+window.mainloop()
